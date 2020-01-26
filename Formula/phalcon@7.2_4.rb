0b0b489381182be512c6f9278269dfcf80b9df6e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-26 00:36:37 +0000

    phalcon@7.2_4: update 4.0.3 bottle.

--- Formula/phalcon@7.2_4.rb
+++ Formula/phalcon@7.2_4.rb
@@ -4,14 +4,14 @@ class PhalconAT724 < AbstractPhp72Extension
   init
   desc "Full-stack PHP framework"
   homepage "https://phalconphp.com/"
-  url "https://github.com/phalcon/cphalcon/archive/v4.0.2.tar.gz"
-  sha256 "db54ad12010e2f38c4a573e4552362f9199bbdd828033546e676bb466c5dbaf0"
+  url "https://github.com/phalcon/cphalcon/archive/v4.0.3.tar.gz"
+  sha256 "4ecad36651f33f7cfa909adedac25fee4527ee44b3843680b63b97898daa07bb"
   head "https://github.com/phalcon/cphalcon.git"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/phalcon"
     cellar :any_skip_relocation
-    sha256 "110b46c19d3e7f3cef0d34dec6d77a04a599157dc1974d7efb2ed3c8d3978623" => :catalina
+    sha256 "a36a1a420f83156fe3d07a266663cb41b86bd08b946b1840d6ab5fd443f25350" => :catalina
   end
 
   depends_on "pcre"
