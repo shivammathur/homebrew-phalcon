622ee59b725e8b57984f33bcff612e95a59e2442
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-14 23:27:49 +0000

    phalcon@7.1_3: update 3.4.5 bottle.

--- Formula/phalcon@7.1_3.rb
+++ Formula/phalcon@7.1_3.rb
@@ -10,6 +10,9 @@ class PhalconAT713 < AbstractPhp71Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/phalcon"
+    cellar :any_skip_relocation
+    rebuild 4
+    sha256 "cc8296d27b1a3aa6746bb74e9248aae4a97aa3338152bc3ef9aba7d08285f597" => :catalina
   end
 
   depends_on "pcre"
