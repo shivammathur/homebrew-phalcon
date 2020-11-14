97d92112ae59bd514ca9eceb099c9a83c34f68d3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-14 01:10:29 +0000

    psr@7.1: update 1.0.1 bottle.

--- Formula/psr@7.1.rb
+++ Formula/psr@7.1.rb
@@ -4,15 +4,14 @@ class PsrAT71 < AbstractPhp71Extension
   init
   desc "PHP extension providing the accepted PSR interfaces "
   homepage "https://github.com/jbboehr/php-psr"
-  url "https://github.com/jbboehr/php-psr/archive/v1.0.0.tar.gz"
-  sha256 "f85be1d1434368abd16e06b81e394487f81b5e2706220f01c85558ba486ee3e7"
+  url "https://github.com/jbboehr/php-psr/archive/v1.0.1.tar.gz"
+  sha256 "d76d62835d6ad22153f3b39cfcc040a2fbb0cf2700173112642131c95f55a27c"
   head "https://github.com/jbboehr/php-psr.git"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/phalcon"
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "962e725bafe51ca0d1ef78169be09268a9766610c63aae432199f6832b5ca273" => :catalina
+    sha256 "3991df8e46009aa7618570521841b9248d8af658e11bf2d65f0df77bd110e1fa" => :catalina
   end
 
   depends_on "pcre"
